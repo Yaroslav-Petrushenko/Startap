c6efd6bbd5edfc13daaa56321a633ddd9ec4b882
Yaroslav-Petrushenko <igorovica@gmail.com>
2022-09-22 16:28:49 +0300

    mobileversion

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,14 @@
 //parallax
 let parallaxStartPos
 const parallaxBG = document.querySelectorAll(".parallax")
-    
+
 function moveBackground(e) {
     // parallaxBG.forEach(element => {
-        let Y = e.pageY - window.pageYOffset - e.target.getBoundingClientRect().top + 1
-        let offsetX = 50 + (e.pageX / window.innerWidth * 15)
+    let Y = e.pageY - window.pageYOffset - e.target.getBoundingClientRect().top + 1
+    let offsetX = 50 + (e.pageX / window.innerWidth * 15)
 
-        let offsetY = 50 + (Y / window.innerHeight * 20)
-        e.target.style.backgroundPosition = `${offsetX}% ${offsetY}%`
+    let offsetY = 50 + (Y / window.innerHeight * 20)
+    e.target.style.backgroundPosition = `${offsetX}% ${offsetY}%`
     // });
 }
 
@@ -16,7 +16,9 @@ parallaxBG.forEach(element => {
     element.style.backgroundPosition = 'center'
     element.style.backgroundSize = '120% auto'
     // element.style.backgroundSizeX = '250%'
-    element.addEventListener("mousemove", function (e) { moveBackground(e); })
+    element.addEventListener("mousemove", function (e) {
+        moveBackground(e);
+    })
     //console.log(element.style)
 });
 
@@ -26,7 +28,7 @@ parallaxBG.forEach(element => {
 let menu = document.querySelectorAll(".top-menu a"),
     interval
 
-function scrollToBlock(href){
+function scrollToBlock(href) {
     let target = document.querySelector(href),
         targetLocation = target.getBoundingClientRect().top + window.pageYOffset, //положення цілі
         currentPosition = window.pageYOffset, //поточна позиція
@@ -37,48 +39,48 @@ function scrollToBlock(href){
         direction = "down"
     } else {
         direction = "up"
-        
+
     }
     clearInterval(interval)
 
 
-    interval = setInterval(()=> {
-        
-        pixelsLeft = Math.abs(targetLocation - window.pageYOffset) 
-        
-        if(pixelScroll <= 45 && pixelsLeft > (window.innerHeight * 0.25)){
+    interval = setInterval(() => {
+
+        pixelsLeft = Math.abs(targetLocation - window.pageYOffset)
+
+        if (pixelScroll <= 45 && pixelsLeft > (window.innerHeight * 0.25)) {
             pixelScroll *= 1.3
         } else if (pixelsLeft < (window.innerHeight * 0.25) && pixelScroll > 3) {
             pixelScroll *= 0.7
         }
-        if(direction == "down"){
+        if (direction == "down") {
             window.scrollTo(0, window.pageYOffset + pixelScroll)
         } else {
             window.scrollTo(0, window.pageYOffset - pixelScroll)
-        } 
+        }
 
         if (Math.abs(window.pageYOffset - targetLocation) <= 3) {
             window.scrollTo(0, targetLocation)
             clearInterval(interval)
             console.log(pixelScroll)
-        } else if (Math.abs(window.pageYOffset + window.innerHeight - document.body.getBoundingClientRect().height) < 5){
+        } else if (Math.abs(window.pageYOffset + window.innerHeight - document.body.getBoundingClientRect().height) < 5) {
             clearInterval(interval)
         }
-    },20)
+    }, 20)
 }
 
 menu.forEach(element => {
     element.onclick = function (event) {
         event.preventDefault()
         scrollToBlock(this.getAttribute("href"))
-    } 
+    }
 })
 
 //hamburger
 let hamburger = document.querySelector(".hamburger")
 let nmenu = document.querySelector(".navmenu")
 hamburger.onclick = function () {
-    nmenu.classList.toggle ("active-burger")
+    nmenu.classList.toggle("active-burger")
 }
 
 
@@ -90,33 +92,32 @@ let firstVisit,
     lastVisit,
     timeInSite,
     firstTimeInSite
-window.onload = function (){
-    if (!localStorage.firstVisit || localStorage.firstVisit == 'undefined'){
-        localStorage.firstVisit = new Date ()
-    } 
+window.onload = function () {
+    if (!localStorage.firstVisit || localStorage.firstVisit == 'undefined') {
+        localStorage.firstVisit = new Date()
+    }
     firstVisit = localStorage.firstVisit
-    if (!localStorage.lastVisit){
+    if (!localStorage.lastVisit) {
         localStorage.lastVisit = firstVisit
     }
-    timeInSite = (new Date() - new Date (localStorage.lastVisit)) / 1000 / 60
-    firstTimeInSite = (new Date () - new Date (localStorage.firstVisit)) / 1000 / 60
-    console.log(firstTimeInSite)
-    if (firstTimeInSite < 5) {
-        console.log ("Welcome to STARTUP")
+    timeInSite = (new Date() - new Date(localStorage.lastVisit)) / 1000 / 60
+
+    if (timeInSite > 5) {
+        console.log("Welcome to STARTUP")
     } else if (timeInSite > 1440) {
-        console.log ("Тебе не було цілий день")
+        console.log("Тебе не було цілий день")
     } else if (timeInSite > 2880) {
-        console.log ("де тебе носило так довго")
+        console.log("де тебе носило так довго")
 
-    window.onblur = function (){
-        localStorage.lastVisit = new Date ()  
-        
+        window.onblur = function () {
+            localStorage.lastVisit = new Date()
+
+        }
+        lastVisit = localStorage.lastVisit
+
+        //console.log(timeInSite)
+
     }
-    lastVisit = localStorage.lastVisit
-    
-    //console.log(timeInSite)
-
-    } 
 }
 // window.addEventListener('click', function (evt) {
 //     if (evt.detail === 3) {
@@ -125,10 +126,10 @@ window.onload = function (){
 // });
 let svgfigur = document.querySelector(".sv svg"),
     trople = document.querySelectorAll(".personal h3")
-    //timer 
-svgfigur.addEventListener('click', function (e){
+//timer 
+svgfigur.addEventListener('click', function (e) {
     if (e.detail === 3) {
-        trople.forEach(element => {  
+        trople.forEach(element => {
             element.innerHTML = 'New text triple click'
         })
     }
@@ -156,14 +157,18 @@ if (animItems.length > 0) {
                 if (!(animItem.classList.contains(`_anim-no`))) {
                     animItem.classList.remove(`_active-anim`)
                 }
-            }            
+            }
         });
     }
+
     function offset(el) {
         const rect = el.getBoundingClientRect()
         let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop
-        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        return {
+            top: rect.top + scrollTop,
+            left: rect.left + scrollLeft
+        }
     }
 
     setTimeout(() => {
@@ -173,23 +178,149 @@ if (animItems.length > 0) {
 
 
 //slider
-function infinitySlider () {
-    let slider = document.querySelector(".slider-card"),
-        cards = document.querySelectorAll(".ap"),
-        btnLeft = document.querySelector(".left"),
-        btnRight = document.querySelector(".right"),
-        heightCards = cards[0].getBoundingClientRect().height,
-        widthCards = cards[0].getBoundingClientRect().width,
-        sliderWidth = slider.getBoundingClientRect().width,
-        cardsCount = Math.floor(sliderWidth / widthCards),
-        distanceCards = (widthCards * cardsCount) / (cardsCount - 1) 
 
-
+let positionCards = 0,
+    cards = document.querySelectorAll(".ap"),
+    slider = document.querySelector(".slider-card"),
+    sliderWidth = slider.getBoundingClientRect().width,
+    widthCards = cards[0].getBoundingClientRect().width,
+    btnLeft = document.querySelector(".left"),
+    btnRight = document.querySelector(".right"),
+    cardsCount = Math.floor(sliderWidth / widthCards),
+    distanceCards = (sliderWidth - (widthCards * cardsCount)) / (cardsCount - 1)
+    
+if (cards.length > cardsCount){
+    positionCards -= (distanceCards + widthCards)
+    let firstEl = cards[cards.length - 1].cloneNode(true)
+    slider.insertAdjacentElement("afterbegin", firstEl)
 }
-window.onresize = infinitySlider ()
+function infinitySlider() {
+    let heightCards = cards[0].getBoundingClientRect().height
+    cards = document.querySelectorAll(".ap")
+    widthCards = cards[0].getBoundingClientRect().width
+    sliderWidth = slider.getBoundingClientRect().width
+    cardsCount = Math.floor(sliderWidth / widthCards)
+    distanceCards = (sliderWidth - (widthCards * cardsCount)) / (cardsCount - 1)
+    slider.style.height = heightCards + "px"
+    if (cards.length > cardsCount){
+        positionCards -= (distanceCards + widthCards)
+        //let firstClone = 
+    }
+    function shuffleCard() {
+        positionCards = 0
+        if (cards.length > cardsCount){
+            positionCards -= (distanceCards + widthCards)
+            btnLeft.style.display = "block"
+            btnRight.style.display = "block"
+
+        } else {
+            btnLeft.style.display = "none"
+            btnRight.style.display = "none"
+        }
+        cards = document.querySelectorAll(".ap")
+        cards.forEach(card => {
+            card.style.left = positionCards + 'px'
+            positionCards += distanceCards + widthCards
+            
+        })
+    }
+    shuffleCard()
+    function changeSlide(direction){
+        if (direction == "left"){
+            cards[cards.length -1].remove()
+            let prelastEl = cards[cards.length - 2].cloneNode(true) // bo pislya remove lenght ne obnovilca
+            slider.insertAdjacentElement("afterbegin", prelastEl)
+            
+        } else if (direction == "right"){
+            cards[0].remove()
+            let preFirstEl = cards[1].cloneNode(true)
+            slider.insertAdjacentElement("beforeend", preFirstEl)
+        }
+        shuffleCard()
+    }
+    
+    btnLeft.onclick = function (){
+        changeSlide ("left")
+    }
+    btnRight.onclick = function (){
+        changeSlide ("right")
+    }
+}
+window.onresize = infinitySlider
+infinitySlider()
+
 
 
 // var div = document.getElementById('div_id'),
 //     clone = div.cloneNode(true)
 //         clone.id = "some_id"
 //     document.body.appendChild(clone)
+// let viewport = document.querySelector(".slider-card").offsetWidth;
+// let btnLeft = document.querySelector(".cards-slider.left")
+// let btnRight = document.querySelector(".cards-slider.right")
+
+// let slides = document.querySelectorAll(".ap")
+// let sliders = []
+
+
+// for (let i = 0; i < slides.length; ++i) {
+//     sliders[i] = slides[i]
+//     slides[i]
+// }
+
+// let offs = 0
+// let step = 0
+
+
+// function drowL() {
+//     let slide = document.createElement("div");
+//     slide.classList.add(".ap")
+//     slide.appendChild(sliders[step])
+//     slide.style.left = offs * viewport + "px"
+//     document.querySelector(".slider-card").appendChild(slide)
+//     if (step + 1 == slide.length) {
+//         step = 0
+//     } else { 
+//         step++
+//     }
+//     offs = 1
+// }
+
+// drowL()
+
+// // function drowR() {
+// //     let slid = document.createElement("div");
+// //     slid.classList.add("ap")
+// //     slid.appendChild(sliders[step])
+// //     slid.style.left = offs * viewport + "px"
+// //     document.querySelector(".slider-card").appendChild(slid)
+// //     if (step == slid.length -1) {
+// //         step = 0
+// //     } else { 
+// //         step--
+// //     }
+// //     offs = 0
+// // }
+// // drowR()
+// let slider = document.querySelector(".slider-card")
+//     cards = document.querySelectorAll(".cards")
+
+// document.querySelector(".cards-slider.left").addEventListener('click', function() {
+//     // offs += 1140
+//     // if(offs > 1052) {
+//     //     offs = 0
+//         drowL()
+//     //}
+//     viewport = offs * viewport - viewport + "px"
+
+// })
+
+// document.querySelector(".cards-slider.right").addEventListener('click', function() {
+//     // offs -= 1140
+//     // if(offs < 0) {
+//     //     offs = -1052
+//         drowL()
+//     //}
+//     viewport = offs * viewport + "px"
+
+// })
---
+++ src/js/slider.js
@@ -0,0 +1,353 @@
+// let sliders = document.querySelector(".slider-card"),
+//     cards = document.querySelectorAll(".ap"),
+//     btnLeft = document.querySelector(".cards-slider.left"),
+//     btnRight = document.querySelector(".cards-slider.right"),
+//     offs = 0,
+//     step = 0,
+//     heightCards = cards[0].getBoundingClientRect().height,
+//     widthCards = cards[0].getBoundingClientRect().width,
+//     sliderWidth = sliders.getBoundingClientRect().width,
+//     cardsCount = Math.floor(sliderWidth / widthCards),
+//     distanceCards = (widthCards * cardsCount) / (cardsCount - 1),
+//     sliderCard = [],
+//     cardsLength = cards.length
+
+// console.log(cards)
+
+// for (let i = 0; i<cardsLength; i++) {
+//   sliderCard[i] = cards[i]
+//   //cards[i]
+//   //console.log(sliderCard[i]);
+// }
+
+// function draw(){
+//     let div = document.createElement('div');
+//     div = sliderCard[sliderCard.length-1];
+//     div.classList.add('.slider-card');
+//     div.style.left = 263 + 'px';
+//     // console.log(div);
+//     sliders.appendChild(div); 
+//     if (step + 1 == sliderCard.length){
+//         step = 0
+//     } else {
+//         step ++
+//     }
+//     offs = 1
+// }
+// draw()
+// draw()
+
+
+
+// function left() {
+//     document.onclick = null;
+//     // 
+//     let slider2 = document.querySelectorAll('.slider-card')
+//     let offset2 = -1;
+//     // console.log(slider2.length);
+//     for (let i = 0; i<slider2.length; i++) {
+//       slider2[i].style.left = offset2*263 - 263 + 'px'
+//       offset2 ++;
+//     }
+//     setTimeout(function() {
+//       slider2[0]
+//       draw()
+//       document.onclick = left
+//     }, 1000)
+    
+// }
+
+// document.onclick = left
+
+
+
+// let items = document.querySelector(".items"),
+//     item = document.querySelectorAll(".item"),
+//     leftArrow = document.querySelector(".cards-slider.left"),
+//     rightArrow = document.querySelector(".cards-slider.right"),
+//     offs = 0,
+//     step = 0,
+//     // heightCards = cards[0].getBoundingClientRect().height,
+//     // widthCards = cards[0].getBoundingClientRect().width,
+//     // sliderWidth = sliders.getBoundingClientRect().width,
+//     // cardsCount = Math.floor(sliderWidth / widthCards),
+//     // distanceCards = (widthCards * cardsCount) / (cardsCount - 1),
+//     // sliderCard = [],
+//     //cardsLength = cards.length,
+//     itemLength = item.length,
+//     slider = []
+//     for (let i = 0; i<itemLength; i++) {
+//       slider[i] = item[i];
+//       // console.log(slider[i]);
+//       //item[i].remove();
+//     }
+    
+    
+//     function burgerSlider() {
+//       let div = document.createElement('div');
+//       div = slider[slider.length-1];
+//       div.classList.add('item');
+//       div.style.left = -263 + 'px';
+//       // console.log(div);
+//       document.querySelector('.items').appendChild(div); 
+      
+      
+//       div = slider[step];
+//       div.classList.add('item');
+//       div.style.left = offs*100 + 'px';
+//       // console.log(div);
+//       document.querySelector('.items').appendChild(div); 
+//       div = slider[step+1];
+//       div.classList.add('item');
+//       div.style.left = offs*263 + 263 + 'px';
+//       // console.log(div);
+//       document.querySelector('.items').appendChild(div); 
+//       offs = 1;
+      
+//     }
+//     function burgerSliderL() {
+//       console.log(step);
+//       if (step == (slider.length-4)) {
+//         step = 1;
+//       } else {
+//         if (step == (slider.length-4)) {
+//           step = 0;
+//         } else {
+//           step = (step +3);
+//         }
+//       }
+//       console.log(step);
+//       let div = document.createElement('div');
+//       div = slider[step];
+//       div.classList.add('item');
+//       div.style.left = offs*100 + 'px';
+//       // console.log(div);
+//       document.querySelector('.items').appendChild(div); 
+        
+//       if (step == 0) {
+//         step = (slider.length-4);
+//       } else {
+//         step = (step - 4);
+//       }
+//       console.log(step);
+//       offs = 1;
+//     }
+    
+//     function left() {
+//       leftArrow.onclick = null;
+//       // 
+//       let slider2 = document.querySelectorAll('.item');
+//       let offs2 = -1;
+//       // console.log(slider2.length);
+//       for (let i = 0; i<slider2.length; i++) {
+//         slider2[i].style.left = offs2*263 - 263 + 'px';
+//         offs2 ++;
+//       }
+//       setTimeout(function() {
+//         slider2[0];
+//         burgerSliderL();
+//         leftArrow.onclick = left;
+//       }, 1000);
+      
+//     }
+    
+//     function burgerSliderR() {
+//       console.log(step);
+//       if (step == 0) {
+//         step = (slider.length-2);
+//       } else {
+//         if (step == 1) {
+//           step = (slider.length-4);
+//         } else {
+//           step = (step -2);
+//         }
+//       }
+//       console.log(step);
+//       let offs = -1;
+//       let div = document.createElement('div');
+//       div = slider[step];
+//       div.classList.add('item');
+//       div.style.left = offs*263 + 'px';
+//       // console.log(div);
+//       document.querySelector('.items').insertBefore(div, items.firstElementChild);
+//       if (step == (slider.length-4)) {
+//           step = 0;
+//        } else {
+//          step = (step+1);
+//        }
+//       console.log(step);
+//       offs = 1;
+//     }
+    
+//     function right() {
+//       rightArrow.onclick = null;
+      
+//       let slider2 = document.querySelectorAll('.item');
+//       let offs2 = (slider2.length-4);
+     
+//       for (let i = (slider2.length-4); i>=0; i--) {
+//         slider2[i].style.left = offs2*263 + 'px';
+//         offs2 --;
+//       }
+//       setTimeout(function() {
+//         slider2[(slider2.length-4)];
+//          burgerSliderR();
+//         rightArrow.onclick = right;
+//       }, 1000);
+//     }
+    
+    
+//     burgerSlider();
+//     step = 0;
+    
+//     leftArrow.onclick = left;
+//     rightArrow.onclick = right;
+
+
+
+
+// let offs = 0,
+//     step = 0
+//     slider = document.querySelector(".slider-card"),
+//     btnLeft = document.querySelector(".slider-card.left"),
+//     btnRight = document.querySelector(".slider-card.right"),
+//     cards = document.querySelectorAll(".ap")
+//     sliderCard = []
+//     //cardsLength = cards.length
+
+
+// for (let i = 0; i<cards.length; i++) {
+//     sliderCard[i] = cards[i]
+//     //cards[i].remove()
+//     //console.log(sliderCard[i])
+//     }
+    
+// function draw(){
+//     let div = document.createElement('div')
+//     div = sliderCard[sliderCard.length - 1]
+//     div.classList.add('.slider-card')
+//     div.style.left = 263 + 'px'
+//     // console.log(div);
+//     slider.appendChild(div)
+//     if (step + 1 == sliderCard.length){
+//         step = 0
+//     } else {
+//         step++
+//     }
+//     offs = 1
+// }
+// draw()
+// draw()
+// function left() {
+//     //document.onclick = null
+    
+//     let slider2 = document.querySelectorAll('.slider-card')
+//     let offset2 = -1
+//     console.log(slider2)
+//     for (let i = 0; i < slider2.length; i++) {
+//         slider2[i].style.left = offset2 * 263 - 263 + 'px'
+//         offset2++;
+//     }
+//     setTimeout(function() {
+//         slider2[0].remove()
+//         draw()
+//         document.onclick = left
+//     }, 1000)
+    
+// }
+
+// document.onclick = left
+
+
+
+
+
+// let viewport = document.querySelector(".slider-card").offsetWidth;
+// let btnLeft = document.querySelector(".cards-slider.left")
+// let btnRight = document.querySelector(".cards-slider.right")
+
+// let slides = document.querySelectorAll(".ap")
+// let sliders = []
+
+
+// for (let i = 0; i < slides.length; ++i) {
+//     sliders[i] = slides[i]
+//     slides[i]
+// }
+
+
+// let step = 0
+// let offs = 0
+ 
+// function drowL() {
+//     let slide = document.createElement("div");
+//     slide.classList.add("ap")
+//     slide.appendChild(sliders[step])
+//     slide.style.left = offs * viewport + "px"
+//     document.querySelector(".slider-card").appendChild(slide)
+//     if (step + 1 == slides.length) {
+//         step = 0
+//     } else { 
+//         step++
+//     }
+//     offs = 1
+// }
+
+// drowL()
+
+// function left() {
+//     btnLeft.removeEventListener('click', left)
+//     let slides2 = document.querySelectorAll(".ap")
+//     let offset2 = 0
+//     for (let i = 0; i < slides2.length; i++) {
+//         slides2[i].style.left = offset2 * viewport - viewport + "px"
+//         offset2++
+//     } 
+//     setTimeout(function () {
+//         slides2[0].remove()
+//         drowL()
+//         btnLeft.addEventListener("click", left)
+
+//     }, 1000)
+// }
+
+
+
+
+
+// function drowR() {
+//     let slide = document.createElement("div");
+//     slide.classList.add("ap")
+//     slide.appendChild(sliders[step])
+//     slide.style.right = offs * viewport + "px"
+//     document.querySelector(".slider-card").appendChild(slide)
+//     if (step + 1 == slides.length) {
+//         step = 0
+//     } else { // Иначе
+//         step--
+//     }
+//     offs = 1
+// }
+
+// drowR()
+
+
+// function right() {
+//     btnRight.addEventListener("click", right)
+//     let slides2 = document.querySelectorAll(".ap")
+//     let offset2 = slides2.length-1
+//     for (let i = 0; i < slides2.length; i++) {
+//         slides2[i].style.right = offset2 * viewport + "px"
+//         offset2--
+//     } 
+//     setTimeout(function () {
+//         slides2[0].remove()
+//         drowR()
+//         btnRight.addEventListener("click", right)
+        
+//     }, 1000)
+// }
+
+
+// btnLeft.addEventListener("click", left)
+// btnRight.addEventListener("click", right)
